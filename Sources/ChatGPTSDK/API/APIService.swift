f20f33a75f97de3b0c1f8735fd1ecbb1657e9796
Tim Wang <overocean@gmail.com>
2023-04-07 09:01:10 +1000

    Add Eidts endpoint & fix date decoding strategy

--- Sources/ChatGPTSDK/API/APIService.swift
+++ Sources/ChatGPTSDK/API/APIService.swift
@@ -10,6 +10,12 @@ public protocol APIService {
 public struct URLSessionAPIService: APIService {
     private let urlSession: URLSession
 
+    private var jsonDecoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        return decoder
+    }
+
     public init(configuration: URLSessionConfiguration = .default,
                 delegate: URLSessionDelegate? = nil,
                 delegateQueue: OperationQueue? = nil) {
@@ -19,17 +25,17 @@ public struct URLSessionAPIService: APIService {
     public func call<E: Endpoint>(endpoint: E) -> AnyPublisher<E.Response, Error> {
         urlSession.dataTaskPublisher(for: endpoint.urlRequest)
             .tryMap { value in
-                try JSONDecoder().decode(E.Response.self, from: value.data)
+                try jsonDecoder.decode(E.Response.self, from: value.data)
             }.eraseToAnyPublisher()
     }
 
     public func call<E: Endpoint>(endpoint: E) async throws -> E.Response {
-        try await JSONDecoder().decode(E.Response.self, from: urlSession.data(for: endpoint.urlRequest).0)
+        try await jsonDecoder.decode(E.Response.self, from: urlSession.data(for: endpoint.urlRequest).0)
     }
 
     public func call<E: Endpoint>(endpoint: E, completion: @escaping (E.Response?, Error?) -> Void) {
         urlSession.dataTask(with: endpoint.urlRequest) { data, _, error in
-            if let data, let object = try? JSONDecoder().decode(E.Response.self, from: data) {
+            if let data, let object = try? jsonDecoder.decode(E.Response.self, from: data) {
                 completion(object, nil)
             } else {
                 completion(nil, error)
---
+++ Sources/ChatGPTSDK/API/Endpoints/EditsEndpoint.swift
@@ -0,0 +1,65 @@
+import Foundation
+
+public struct EditsEndpoint: Endpoint {
+    public struct Request: Codable {
+        public let model: GPTModel
+        public let input: String?
+        public let instruction: String
+        public let n: Int?
+        public let temperature: Float?
+        public let topP: Float?
+
+        enum CodingKeys: String, CodingKey {
+            case model
+            case input
+            case instruction
+            case n
+            case temperature
+            case topP = "top_p"
+        }
+
+        public init(model: GPTModel = .text_davinci_edit_001,
+                    input: String? = nil,
+                    instruction: String,
+                    n: Int? = nil,
+                    temperature: Float? = nil,
+                    topP: Float? = nil,
+                    stream: Bool? = nil,
+                    stop: String? = nil) {
+            self.model = model
+            self.input = input
+            self.instruction = instruction
+            self.n = n
+            self.temperature = temperature
+            self.topP = topP
+        }
+    }
+
+    public struct Response: Codable {
+        public struct Choice: Codable {
+            public let text: String
+            public let index: Int
+        }
+
+        public let object: String
+        public let created: Date
+        public let choices: [Choice]
+        public let usage: Usage
+    }
+    
+    public private(set) var urlRequest: URLRequest = {
+        var request = URLRequest(url: URL(string: "https://api.openai.com/v1/edits")!)
+        request.httpMethod = "POST"
+        return request
+    }()
+
+    public let request: Request
+
+    public init(request: Request) throws {
+        self.request = request
+        commonHeader.forEach { key, value in
+            urlRequest.setValue(value, forHTTPHeaderField: key)
+        }
+        urlRequest.httpBody = try JSONEncoder().encode(request)
+    }
+}
